2967b94175d355417d037ed43e572eeb5c09a9dd
Mageln <mageln3321@vk.com>
2024-09-19 13:47:35 +0300

    dev

--- src/app/profile/Profile.jsx
+++ src/app/profile/Profile.jsx
@@ -62,67 +62,69 @@ const Profile = () => {
   };
 
   return (
-    <div className=" mx-auto ">
+    <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+      <div className="bg-white shadow-md rounded p-4">
+        <h1 className="text-3xl font-bold mb-4">
+          Профиль
+        </h1>
+        <p className="text-lg mb-2"> <b> Пользователь:</b> {user.username}</p>
+        <p className="text-lg mb-4"> <b>Почта: </b>  {user.email}</p>
 
-      <h1 className="text-3xl font-bold mb-4">
-        Профиль
-      </h1>
-      <p className="text-lg mb-2"> <b> Пользователь:</b> {user.username}</p>
-      <p className="text-lg mb-4"> <b>Почта: </b>  {user.email}</p>
-
-      <h2 className="text-2xl font-bold mb-4">Заметки</h2>
-      <AddNote addRecord={addRecord} />
-      <ul className="list-none mb-4">
-        {records.map((record) => (
-          <div key={record.id} className="mt-5 bg-white shadow-md rounded px-4 py-2 w-300px h-200px">
-            {editingRecord && editingRecord.id === record.id ? (
-              <RecordEditor
-                record={record}
-                onSave={handleSaveChanges}
-                onCancel={handleCancelEdit}
-              />
-            ) : (
-              <div className=" mt- items-center">
-                <span className="cursor-pointer flex justify-between items-center mb-2" onClick={() => handleToggleDescription(record)}>
-                  <h3 className='text-lg font-bold'>
-                    {record.title}
-                  </h3>
-                </span>
-                <hr className="border-gray-200 mb-2" />
-                {
-                  openRecords[record.id] && (
-                    <div>
-                      <textarea
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        placeholder="Введите описание"
-                        className="w-full h-20 p-2 mb-2 border border-gray-300 rounded"
-                      />
-                      <button
-                        onClick={() => handleAddDescription(record)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                      >
-                        Сохранить описание
-                      </button>
-                     
-                     
-                    </div>
-                  )
-                }
-                <div className="text-gray-600">{record.description}</div>
-                <div className="flex items-center -top-70px left-55px relative">
-                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDeleat(record)} ><FaRegTrashAlt /></button>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2" onClick={() => handleEditRecord(record)}>
-                    <MdOutlineEdit />
-                  </button>
+        <h2 className="text-2xl font-bold mb-4">Заметки</h2>
+        <AddNote addRecord={addRecord} />
+        <ul className="list-none mb-4">
+          {records.map((record) => (
+            <div key={record.id} className="mt-5 bg-white shadow-md rounded px-4 py-2 w-300px h-200px">
+              {editingRecord && editingRecord.id === record.id ? (
+                <RecordEditor
+                  record={record}
+                  onSave={handleSaveChanges}
+                  onCancel={handleCancelEdit}
+                />
+              ) : (
+                <div className=" mt- items-center">
+                  <span className="cursor-pointer flex justify-between items-center mb-2" onClick={() => handleToggleDescription(record)}>
+                    <h3 className='text-lg font-bold'>
+                      {record.title}
+                    </h3>
+                  </span>
+                  <hr className="border-gray-200 mb-2" />
+                  {
+                    openRecords[record.id] && (
+                      <div>
+                        <textarea
+                          value={description}
+                          onChange={(e) => setDescription(e.target.value)}
+                          placeholder="Введите описание"
+                          className="w-full h-20 p-2 mb-2 border border-gray-300 rounded"
+                        />
+                        <button
+                          onClick={() => handleAddDescription(record)}
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                          Сохранить описание
+                        </button>
+                       
+                       
+                      </div>
+                    )
+                  }
+                  <div className="text-gray-600">{record.description}</div>
+                  <div className="flex items-center mt-1 -top-70px left-55px relative">
+                    <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={() => handleDeleat(record)} ><FaRegTrashAlt /></button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2" onClick={() => handleEditRecord(record)}>
+                      <MdOutlineEdit />
+                    </button>
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
-      </ul>
+              )}
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
+
 
 export default Profile
